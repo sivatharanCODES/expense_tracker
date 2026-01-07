70e13b5cd6a5d1cd48c643b5b9f110f75ab5db7f
Sivatharan <sivatharanoff97@gmail.com>
2026-01-07 05:43:47 +0530

    add dismissible to list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -43,6 +43,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +67,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text("The cart"),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
