2c24684b1fe84682abe1a1697f8c083f902c2354
Sivatharan <sivatharanoff97@gmail.com>
2026-01-08 10:36:56 +0530

    add chart

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 // Todo: Add Expenses wudget
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -82,7 +83,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("The cart"),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: ExpensesList(
               expenses: _registeredExpenses,
